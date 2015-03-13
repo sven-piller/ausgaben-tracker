c24a68d884d2504a4b1953c4cd4404d37ea98a2b
Sven Piller <sven.piller@gmail.com>
2015-03-13 23:59:15 +0100

    Create-Seite erstellt

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -7,65 +7,76 @@
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+	$stateProvider
 
-    .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
+		.state('app', {
+		url: "/app",
+		abstract: true,
+		templateUrl: "templates/menu.html",
+		controller: 'AppCtrl'
+	})
 
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
+	.state('app.search', {
+		url: "/search",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/search.html"
+			}
+		}
+	})
 
-  .state('app.browse', {
-      url: "/browse",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.itemlists', {
-      url: "/itemlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/itemlists.html",
-          controller: 'ItemlistsCtrl'
-        }
-      }
-    })
+	.state('app.browse', {
+		url: "/browse",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/browse.html"
+			}
+		}
+	})
 
-  .state('app.single', {
-    url: "/itemlists/:itemId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/item.html",
-        controller: 'ItemCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/itemlists');
+	.state('app.itemlists', {
+		url: "/itemlists",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/itemlists.html",
+				controller: 'ItemlistsCtrl'
+			}
+		}
+	})
+
+	.state('app.create', {
+		url: "/itemlists/create",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/create.html",
+				controller: 'ItemCreateCtrl'
+			}
+		}
+	})
+
+	.state('app.single', {
+		url: "/itemlists/:itemId",
+		views: {
+			'menuContent': {
+				templateUrl: "templates/item.html",
+				controller: 'ItemCtrl'
+			}
+		}
+	});
+	// if none of the above states are matched, use this as the fallback
+	$urlRouterProvider.otherwise('/app/itemlists');
 });
--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,58 +1,78 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-	// Form data for the login modal
-	$scope.loginData = {};
+  // Form data for the login modal
+  $scope.loginData = {};
 
-	// Create the login modal that we will use later
-	$ionicModal.fromTemplateUrl('templates/login.html', {
-		scope: $scope
-	}).then(function(modal) {
-		$scope.modal = modal;
-	});
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
 
-	// Triggered in the login modal to close it
-	$scope.closeLogin = function() {
-		$scope.modal.hide();
-	};
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
 
-	// Open the login modal
-	$scope.login = function() {
-		$scope.modal.show();
-	};
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
 
-	// Perform the login action when the user submits the login form
-	$scope.doLogin = function() {
-		console.log('Doing login', $scope.loginData);
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
 
-		// Simulate a login delay. Remove this and replace with your login
-		// code if using a login system
-		$timeout(function() {
-			$scope.closeLogin();
-		}, 1000);
-	};
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
 })
 
 .controller('ItemlistsCtrl', function($scope) {
-	$scope.itemlists = [{
-		title: 'Reggae',
-		id: 1
-	}, {
-		title: 'Chill',
-		id: 2
-	}, {
-		title: 'Dubstep',
-		id: 3
-	}, {
-		title: 'Indie',
-		id: 4
-	}, {
-		title: 'Rap',
-		id: 5
-	}, {
-		title: 'Cowbell',
-		id: 6
-	}];
+  $scope.itemlists = [{
+    id: 1,
+    date: '2015-03-01',
+    title: 'Schrank',
+    merchant: 'Ikea',
+    amount: '20.23'
+  }, {
+    id: 2,
+    date: '2015-03-02',
+    title: 'Buch',
+    merchant: 'Amazon',
+    amount: '20.23'
+  }, {
+    id: 3,
+    date: '2015-03-03',
+    title: 'Tisch',
+    merchant: 'Lidl',
+    amount: '20.23'
+  }, {
+    id: 4,
+    date: '2015-03-04',
+    title: 'Lebensmittel',
+    merchant: 'Aldi',
+    amount: '20.23'
+  }, {
+    id: 5,
+    date: '2015-03-05',
+    title: 'Tanken',
+    merchant: 'Agip',
+    amount: '20.23'
+  }, {
+    id: 6,
+    date: '2015-03-06',
+    title: 'Urlaub',
+    merchant: 'Lufthansa',
+    amount: '20.23'
+  }];
 })
 
-.controller('ItemCtrl', function($scope, $stateParams) {});
+.controller('ItemCtrl', function($scope, $stateParams) {})
+
+.controller('ItemCreateCtrl', function($scope, $stateParams) {});
