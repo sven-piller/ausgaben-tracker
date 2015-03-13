827e642adafc04bd41a9d664aba371cc53ef4b75
Sven Piller <sven.piller@gmail.com>
2015-03-13 22:11:10 +0100

    simple template

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -3,17 +3,45 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('app', ['ionic'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+	// Ionic uses AngularUI Router which uses the concept of states
+	// Learn more here: https://github.com/angular-ui/ui-router
+	// Set up the various states which the app can be in.
+	// Each state's controller can be found in controllers.js
+	$stateProvider
+
+		.state('page7', {
+		url: '/list',
+		templateUrl: 'page7.html'
+	})
+
+	.state('page8', {
+		url: '/create',
+		templateUrl: 'page8.html'
+	});
+
+	// if none of the above states are matched, use this as the fallback
+
+	$urlRouterProvider.otherwise('/list');
+
+
+});
